9ca6b42be2f2204d83c73d0f84c59551124fead8
Colin Piper <colin_piper@hotmail.co.uk>
2023-09-29 20:02:29 +0100

    Cjinconsulting/restructure root 19 (#20)
    * #19 add directory tree
    
    * Update week1.md
    
    * change structure of root module and add tagging to bucket

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-
-  cloud {
-    organization = "colin_piper"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-}
-
-provider "random" {
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs
 resource "random_string" "bucket_name" {
   lower   = true
@@ -39,8 +11,8 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
