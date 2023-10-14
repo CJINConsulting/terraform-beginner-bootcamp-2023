3a9dea9a8bf453b9356332d840708b4ea030d39f
Colin Piper <colin_piper@hotmail.co.uk>
2023-10-14 16:00:04 +0100

    #48 fixed issues with provider, set up 2 different towns (#49)

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,7 +5,7 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:vDWdz2cWmGmDvH3Q5rwRfM27y/JciA0vd9nwNBnKSV4=",
+    "h1:C4woZVFXdtNbV0gUycv/pcNgHSPEQ8r4n26juuCQx78=",
   ]
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value       = module.terrahouse_aws.bucket_name
+  value       = module.home_swos_hosting.bucket_name
 }
 
 output "s3_website_endpoint" {
-  value = module.terrahouse_aws.website_endpoint
+  value = module.home_swos_hosting.website_endpoint
 }
 
-output "cloudfront_url" {
+output "domain_name" {
   description = "The cloudfront distribution domain"
-  value       = module.terrahouse_aws.cloudfront_url
+  value       = module.home_swos_hosting.domain_name
 }
